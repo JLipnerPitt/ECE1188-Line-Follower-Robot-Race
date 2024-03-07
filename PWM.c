524fa82b92ea9a556616f08916c6e0e1d18b9ed7
Seth <94641030+SethNor@users.noreply.github.com>
2024-03-06 21:27:24 -0500

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -1,61 +1,56 @@
-// TimerA0.c
+// PWM.c
 // Runs on MSP432
-// Use Timer A1 in periodic mode to request interrupts at a particular
-// period.
+// PWM on P2.4 using TimerA0 TA0.CCR1
+// PWM on P2.5 using TimerA0 TA0.CCR2
+// PWM on P2.6 using TimerA0 TA0.CCR3
+// PWM on P2.7 using TimerA0 TA0.CCR4
+// MCLK = SMCLK = 3MHz DCO; ACLK = 32.768kHz
+// TACCR0 generates a square wave of freq ACLK/1024 =32Hz
+// Derived from msp432p401_portmap_01.c in MSPware
 
-#include <stdint.h>
+
 #include "msp.h"
 
-void (*TimerA0Task)(void);   // user function
-
-// ***************** TimerA0_Init ****************
-// Activate Timer A0 interrupts to run user task periodically
-// Inputs:  task is a pointer to a user function
-//          period in units (24/SMCLK), 16 bits
+//***************************PWM_Init34*******************************
+// PWM outputs on P2.6, P2.7
+// Inputs:  period (1.333us)
+//          duty3
+//          duty4
 // Outputs: none
-void TimerA0_Init(void(*task)(void), uint16_t period){
-  TimerA0Task = task;             // user function
-  TIMER_A0->CTL &= ~0x0030;       // halt Timer A0
-  // bits15-10=XXXXXX, reserved
-  // bits9-8=10,       clock source to SMCLK
-  // bits7-6=10,       input clock divider /4
-  // bits5-4=00,       stop mode
-  // bit3=X,           reserved
-  // bit2=0,           set this bit to clear
-  // bit1=0,           no interrupt on timer
-  TIMER_A0->CTL = 0x0280;
-  // bits15-14=00,     no capture mode
-  // bits13-12=XX,     capture/compare input select
-  // bit11=X,          synchronize capture source
-  // bit10=X,          synchronized capture/compare input
-  // bit9=X,           reserved
-  // bit8=0,           compare mode
-  // bits7-5=XXX,      output mode
-  // bit4=1,           enable capture/compare interrupt on CCIFG
-  // bit3=X,           read capture/compare input from here
-  // bit2=0,           output this value in output mode 0
-  // bit1=X,           capture overflow status
-  // bit0=0,           clear capture/compare interrupt pending
-  TIMER_A0->CCTL[0] = 0x0010;
-  TIMER_A0->CCR[0] = (period - 1);   // compare match value
-  TIMER_A0->EX0 = 0x0005;    // configure for input clock divider /6
-// interrupts enabled in the main program after all devices initialized
-  NVIC->IP[2] = (NVIC->IP[2]&0xFFFFFF00)|0x00000040; // priority 2
-  NVIC->ISER[0] = 0x00000100;   // enable interrupt 8 in NVIC
-  TIMER_A0->CTL |= 0x0014;      // reset and start Timer A0 in up mode
+// SMCLK = 48MHz/4 = 12 MHz, 83.33ns
+// Counter counts up to TA0CCR0 and back down
+// Let Timerclock period T = 8/12MHz = 666.7ns
+// period of P7.3 squarewave is 4*period*666.7ns
+// P2.6=1 when timer equals TA0CCR3 on way down, P2.6=0 when timer equals TA0CCR3 on way up
+// P2.7=1 when timer equals TA0CCR4 on way down, P2.7=0 when timer equals TA0CCR4 on way up
+// Period of P2.6 is period*1.333us, duty cycle is duty3/period
+// Period of P2.7 is period*1.333us, duty cycle is duty4/period
+void PWM_Init34(uint16_t period, uint16_t duty3, uint16_t duty4){
+
+  P2->DIR |= 0xC0;          // P2.6, P2.7 output
+  P2->SEL0 |= 0xC0;         // P2.6, P2.7 Timer0A functions
+  P2->SEL1 &= ~0xC0;        // P2.6, P2.7 Timer0A functions
+  TIMER_A0->CCTL[0] = 0x0080;      // CCI0 toggle
+  TIMER_A0->CCR[0] = period-1;       // Period is 2*period*8*83.33ns is 1.333*period
+  TIMER_A0->EX0 = 0x0000;          // divide by 1
+  TIMER_A0->CCTL[3] = 0x0040;      // CCR3 toggle/reset
+  TIMER_A0->CCR[3] = duty3;        // CCR3 duty cycle is duty3/period
+  TIMER_A0->CCTL[4] = 0x0040;      // CCR4 toggle/reset
+  TIMER_A0->CCR[4] = duty4;        // CCR4 duty cycle is duty4/period
+  TIMER_A0->CTL = 0x02F0;          // SMCLK=12MHz, divide by 8, up-down mode
 }
 
+//***************************PWM_Duty3*******************************
+// change duty cycle of PWM output on P2.6
+// Inputs:  duty3
+// Outputs: none
+// period of P2.6 is 2*period*666.7ns, duty cycle is duty3/period
+//***************************PWM_Duty4*******************************
+// change duty cycle of PWM output on P2.7
+// Inputs:  duty4
+// Outputs: none// period of P2.7 is 2*period*666.7ns, duty cycle is duty2/period
+void PWM_Duty34(uint16_t Duty3, uint16_t Duty4){
 
-// ------------TimerA0_Stop------------
-// Deactivate the interrupt running a user task periodically.
-// Input: none
-// Output: none
-void TimerA0_Stop(void){
-  TIMER_A0->CTL &= ~0x0030;       // halt Timer A0
-  NVIC->ICER[0] = 0x00000100;     // disable interrupt 8 in NVIC
-}
-
-void TA0_0_IRQHandler(void){
-  TIMER_A0->CCTL[0] &= ~0x0001; // acknowledge capture/compare interrupt 0
-  (*TimerA0Task)();             // execute user task
+  TIMER_A0->CCR[4] = Duty4;        // CCR4 duty cycle is duty4/period
+  TIMER_A0->CCR[3] = Duty3;       // CCR3 duty cycle is duty3/period
 }
